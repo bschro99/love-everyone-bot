8e03451bf79c1c856b0ad2aca855ed57b5fb9e15
bschro99 <31928914+bschro99@users.noreply.github.com>
2018-03-01 11:08:48 -0500

    Adding files via upload

---
+++ credentials.py
@@ -0,0 +1,12 @@
+#Import Tweepy
+import tweepy
+
+
+#Your private credentials
+#These can be found on https://www.apps.twitter.com
+
+consumer_key = 'enter your consumer key here'
+consumer_secret = 'enter your secret consumer key here'
+access_token = 'enter your access token here'
+access_token_secret = 'enter your secret access token here'
+
